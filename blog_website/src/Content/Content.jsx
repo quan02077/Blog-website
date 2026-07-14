74c3aa8083047414335b04769ac6db9cff203e65
Quan Nguyen <quan02077@gmail.com>
2026-07-14 14:23:45 +0700

    Chỉnh sửa bố cục content

--- blog_website/src/Content/Content.jsx
+++ blog_website/src/Content/Content.jsx
@@ -1,7 +1,7 @@
 function Content() {
     return (
-        <div className="grid grid-cols-4 gap-4 mt-3">
-            <div className="col-span-1 bg-blue-500">
+        <div className="grid grid-cols-4 gap-4 my-4 mx-20">
+            <div className="col-span-1 w-full h-full bg-blue-500">
                 sideBar
             </div>
             <div className="col-span-2 bg-green-500">
